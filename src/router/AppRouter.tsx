417a6877c14a5701fde68fe396d4f85efe8a53f5
Jocho14 <jochotyler1@gmail.com>
2025-03-23 12:37:16 +0100

    fix: downgrade to react 18

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,4 +1,4 @@
-import { Suspense, useEffect } from "react";
+import { Suspense } from "react";
 import { Routes, Route } from "react-router-dom";
 // import useScrollTop from "@/hooks/useScrollTop";
 import routes from "./routes";
